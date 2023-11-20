f29e05060bdccfd774721732cd8e40ec2572bafd
Petrik Diak <diak@petrik.hu>
2023-11-20 17:55:08 +0100

    Forcast function

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { CurrentWeather } from './CWInterface';
+import { WeatherForcast } from './WFInterface';
 import './style.css'
 
 async function load(){
@@ -16,6 +17,20 @@ async function load(){
     (document.getElementById("homerseklet")! as HTMLInputElement).value = content.current.temp_c.toString();
     (document.getElementById("felhozet")! as HTMLInputElement).value = content.current.condition.text;
     document.getElementById("icon")!.setAttribute("src", "//cdn.weatherapi.com/weather/64x64/night/113.png");
+    forcast();
+}
+
+async function forcast() {
+  let result = await fetch("http://api.weatherapi.com/v1/forecast.json?key=638048ba6a1345db95d145029232011&q=Budapest&days=3&aqi=no&alerts=no")
+  console.log("Status: " + result.status);
+  console.log("Success?: " + result.ok);
+  let content = await result.json() as WeatherForcast;
+
+  //content.forecast.forecastday[x].hour[y]
+  //x = hanyadik napi előrejelzés
+  //y = nap hanyadik órája
+  console.log(content.forecast.forecastday[1].hour[0].condition.text);
+  
 }
 
 document.addEventListener("DOMContentLoaded", load);
