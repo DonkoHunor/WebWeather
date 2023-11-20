35c3f707b86dfcb6601380660c8d21588bab8b64
Petrik Diak <diak@petrik.hu>
2023-11-20 17:27:48 +0100

    Created interface for API

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
+import { RootObject } from './Interface';
 import './style.css'
 
-function init(){
+async function load(){
+    let result = await fetch("https://api.weatherapi.com/v1/current.json?key=638048ba6a1345db95d145029232011&q=Budapest&aqi=no");
+    console.log("Status: " + result.status);
+    console.log("Success?: " + result.ok);
+    
+    let content = await result.json() as RootObject;
+    console.log(content.location.name);
+    console.log(content.current.condition.text);
+    console.log(content.current.temp_c);
+    console.log(content.current.condition.icon);
+    
     
 }
 
-document.addEventListener("DOMContentLoaded", init);
+document.addEventListener("DOMContentLoaded", load);
